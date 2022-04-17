5a318a10e76036a6f559770fb9bb8eeaa33b0ff5
vijaygithubrep <100856776+vijaygithubrep@users.noreply.github.com>
2022-04-17 13:07:04 +0530

    Add files via upload

--- models.py
+++ models.py
@@ -1,12 +1,3 @@
 from django.db import models
 
-
-class Book(models.Model):
-    name = models.CharField(max_length=550)
-    edition = models.IntegerField(max_length=100)
-    author = models.CharField(max_length=250, blank=True)
-    price = models.IntegerField(max_length=10)
-
-    def __str__(self):
-        return self.name
-
+# Create your models here.
